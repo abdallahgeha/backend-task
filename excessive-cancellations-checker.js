6c2301d05c63e9406cd91ba80017b74aec1a9e17
Abdallah Geha <geha.abdallah@gmail.com>
2024-11-11 14:48:54 +0100

    readfile line by line

--- excessive-cancellations-checker.js
+++ excessive-cancellations-checker.js
@@ -1,27 +1,44 @@
-export class ExcessiveCancellationsChecker {
+import fs from "fs";
+import readline from "readline/promises";
 
-    /* 
+export class ExcessiveCancellationsChecker {
+  /* 
         We provide a path to a file when initiating the class
         you have to use it in your methods to solve the task
     */
-    constructor(filePath) {
-        this.filePath = filePath;
-    }
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.#loadFile();
+  }
 
-    /**
-    * Returns the list of companies that are involved in excessive cancelling.
-    * Note this should always resolve an array or throw error.
-    */
-    async companiesInvolvedInExcessiveCancellations() {
-        //TODO Implement...
-    }
+  /**
+   * Returns the list of companies that are involved in excessive cancelling.
+   * Note this should always resolve an array or throw error.
+   */
+  async companiesInvolvedInExcessiveCancellations() {
+    //TODO Implement...
+  }
 
-    /**
-     * Returns the total number of companies that are not involved in any excessive cancelling.
-     * Note this should always resolve a number or throw error.
-    */
-    async totalNumberOfWellBehavedCompanies() {
-        //TODO Implement...
-    }
-    
+  /**
+   * Returns the total number of companies that are not involved in any excessive cancelling.
+   * Note this should always resolve a number or throw error.
+   */
+  async totalNumberOfWellBehavedCompanies() {
+    //TODO Implement...
+  }
+
+  #loadFile() {
+    const readInterface = readline.createInterface({
+      input: fs.createReadStream(this.filePath),
+      crlfDelay: Infinity,
+    });
+
+    readInterface.on("line", (line) => {
+      console.log(line);
+    });
+
+    readInterface.on("close", () => {
+      console.log("closed");
+    });
+  }
 }
